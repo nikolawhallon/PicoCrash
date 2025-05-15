fe36178907b3f474611ec36d6a5f685acd0cbc37
Nikola Whallon <vacuumbrewstudios@gmail.com>
2025-05-15 14:20:01 -0700

    added conke icon

--- Scenes/PlayerSubViewport.gd
+++ Scenes/PlayerSubViewport.gd
@@ -1,4 +1,4 @@
 extends SubViewportContainer
 
 func _on_player_score_changed(score):
-	$SubViewport/MarginContainer/Label.text = str("SCORE ", score)
+	$SubViewport/MarginContainer/HBoxContainer/Label.text = str("SCORE ", score)
