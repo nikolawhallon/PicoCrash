83bebcea056f7d5f09e4c78911cc2c9da4b3faac
Nikola Whallon <vacuumbrewstudios@gmail.com>
2025-05-15 21:14:32 -0700

    configurable number of players!

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -31,5 +31,79 @@ func _ready():
 		warning.position.x = -48
 		add_child(warning)
 
+	var player_num = 2
+	
+	if player_num == 2:
+		var player1 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player1.player = 1
+		player1.size = Vector2(640, 240)
+		player1.get_node("SubViewport").size = Vector2(640, 240)
+		player1.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 20)
+		add_child(player1)
+
+		var player2 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player2.player = 2
+		player2.size = Vector2(640, 240)
+		player2.get_node("SubViewport").size = Vector2(640, 240)
+		player2.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 15)
+		player2.position = Vector2(0, 240)
+		add_child(player2)
+
+	if player_num == 3:
+		var player1 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player1.player = 1
+		player1.size = Vector2(320, 240)
+		player1.get_node("SubViewport").size = Vector2(320, 240)
+		player1.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 20)
+		add_child(player1)
+
+		var player2 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player2.player = 2
+		player2.size = Vector2(320, 240)
+		player2.get_node("SubViewport").size = Vector2(320, 240)
+		player2.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 15)
+		player2.position = Vector2(320, 0)
+		add_child(player2)
+
+		var player3 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player3.player = 3
+		player3.size = Vector2(320, 240)
+		player3.get_node("SubViewport").size = Vector2(320, 240)
+		player3.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 10)
+		player3.position = Vector2(0, 240)
+		add_child(player3)
+
+	if player_num == 4:
+		var player1 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player1.player = 1
+		player1.size = Vector2(320, 240)
+		player1.get_node("SubViewport").size = Vector2(320, 240)
+		player1.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 20)
+		add_child(player1)
+
+		var player2 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player2.player = 2
+		player2.size = Vector2(320, 240)
+		player2.get_node("SubViewport").size = Vector2(320, 240)
+		player2.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 15)
+		player2.position = Vector2(320, 0)
+		add_child(player2)
+
+		var player3 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player3.player = 3
+		player3.size = Vector2(320, 240)
+		player3.get_node("SubViewport").size = Vector2(320, 240)
+		player3.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 10)
+		player3.position = Vector2(0, 240)
+		add_child(player3)
+		
+		var player4 = load("res://Scenes/PlayerSubViewport.tscn").instantiate()
+		player4.player = 4
+		player4.size = Vector2(320, 240)
+		player4.get_node("SubViewport").size = Vector2(320, 240)
+		player4.get_node("SubViewport").get_node("Player").global_position = Vector3(10, 2, 5)
+		player4.position = Vector2(320, 240)
+		add_child(player4)
+	
 func _process(delta):
 	pass
--- Scenes/PlayerSubViewport.gd
+++ Scenes/PlayerSubViewport.gd
@@ -1,4 +1,9 @@
 extends SubViewportContainer
 
+@export var player: int = 1
+
+func _ready():
+	$SubViewport/Player.player = player
+
 func _on_player_score_changed(score):
-	$SubViewport/MarginContainer/HBoxContainer/Label.text = str("SCORE ", score)
+	$SubViewport/MarginContainer/StatsContainer/Score.text = str("SCORE ", score)
